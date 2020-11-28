be5a8bbd5932df12d4de523541f0e8d01129e2a4
ExcelDurant <gnopaexcel@yahoo.fr>
2020-11-28 13:48:58 +0100

    making the code more efficient

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,46 +1,38 @@
 
-var question_1 = {
-    question: "Who was the first president of the US?",
-    answer_1: "Abraham Lincoln",
-    answer_2: "Abrahom Lincoln",
-    answer_3: "Abrahem Lincoln",
-    answer_4: "Abrahim Lincoln",
-    correctAns: "Abraham Lincoln"
-}
-
-var question_2 = {
-    question: "The name of the longest river in the world",
-    answer_1: "river Nile",
-    answer_2: "river Sanag",
-    answer_3: "river Jam",
-    answer_4: "river Dibam",
-    correctAns: "river Nile"
-}
-
-var question_3 = {
-    question: "The largest lake in the world is?",
-    answer_1: "lake Vecta",
-    answer_2: "lake Nyos",
-    answer_3: "lake Sama",
-    answer_4: "lake Superior",
-    correctAns: "lake Superior"
-}
-
-var question_4 = {
-    question: "What is the tallest mountain in Africa",
-    answer_1: "mt Alaska",
-    answer_2: "mt Kilamanjero",
-    answer_3: "mt Everest",
-    answer_4: "mt Cameroon",
-    correctAns: "mt Kilamanjero"
-}
-
-
 // create an array containing all the questions
-var questions = [];
-for (let i = 0; i < 5; i++) {
-    questions.push(`question_${i}`);
-}
+var questions = [
+    {
+        question: "Who was the first president of the US?",
+        answers: ["Abraham Lincoln", "Abraham Lincon", "Abrahom Lincoln", "Abraham Lancoln"],
+        correctAns: "Abraham Lincoln"
+    },
+
+   {
+        question: "The name of the longest river in the world",
+        answers: ["river Nile", "river Sanag", "river Jam", "river Dibam"],
+        correctAns: "river Nile"
+    },
+    
+    {
+        question: "The largest lake in the world is?",
+        answers: ["lake Vecta", "lake Nyos", "lake Sama", "lake Superior"],
+        correctAns: "lake Superior"
+    },
+    
+    {
+        question: "What is the tallest mountain in Africa",
+        answers: ["mt Alaska", "mt Kilamanjero", "mt Everest", "mt Cameroon"],
+        correctAns: "mt Kilamanjero"
+    },
+    
+    {
+        question: "who are you?",
+        answers: ["me","you","I don know","you and me"],
+        correctAns: "me"
+    }
+    
+]
+
 console.log(questions);
 
 var numberOfQuestions = questions.length;
@@ -53,26 +45,21 @@ function questionaire() {
     function showQuestion() {
 
         // converts the string in the array of questions into a variable
-        let val = Object.values(window[questions[Math.ceil(Math.random() * (numberOfQuestions-1))]]);
+        let val = Object.values(questions[Math.floor(Math.random() * (numberOfQuestions))]);
 
         // Displays the question and the answers without displaying the correct answer
-        for (let i = 0; i < 5; i++) {
-            if (i < 5 && i > 0) {
-                console.log(i + ") " + val[i])
-            } else if (i == 0) {
-                console.log(val[i])
-            } else {
-                break;
-            }
+        console.log(val[0])
+        for (let i = 0; i < 4; i++) {
+            console.log((i+1)+")"+val[1][i])
         }
 
         correctAns();
 
         // checks if the answer is correct or wrong
         function correctAns() {
-            let choice = Number(prompt("choose 1,2,3 or 4"));
-            let answer = val[choice];
-            if (answer == val[5]) {
+            let choice = Number(prompt("choose 1,2,3 or 4")) - 1;
+            let answer = val[1][choice];
+            if (answer == val[2]) {
                 console.log("You have chosen the correct answer");
                 // Adding points after successfully answering
                 points += 1;
@@ -90,4 +77,3 @@ function questionaire() {
 }
 
 questionaire();
-
