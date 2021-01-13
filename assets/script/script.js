b3dd60913e79b83cb38f40059c9f547f6ce25ef1
ExcelDurant <gnopaexcel@yahoo.fr>
2021-01-13 19:51:07 +0100

    adding more questions

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,12 +28,6 @@ var questions = [
     },
 
     {
-        question: "who are you?",
-        answers: ["me", "you", "I don know", "you and me"],
-        correctAns: "me"
-    },
-
-    {
         question: "What is the chemical formula for Table Salt?",
         answers: ["CuSO4", "MgSO4", "NaCl", "NaSO4"],
         correctAns: "NaCl"
@@ -263,6 +257,252 @@ var questions = [
         question:"How many permanent teeth does a dog have?",
         answers: ["42", "56", "30", "25"],
         correctAns:"42"
+    },
+
+    {
+        question:"What’s longer, a nautical mile or a mile?",
+        answers:["mile", "Nautical mile", "both", "not know"],
+        correctAns:"Nautical mile"
+    },
+
+    {
+        question:"Which country in the world is believed to have the most miles of motorway?(",
+        answers:["USA", "Germany", "Japan", "China"],
+        correctAns:"China"
+    },
+
+    {
+        question:"In what decade was pop icon Madonna born?",
+        answers:["1950s", "1960s", "1980s", "1970s"],
+        correctAns:"1950s"
+    },
+
+    {
+        question:"What is the capital city of Australia?",
+        answers:["Darwin", "Sydney", "Perth", "Canberra"],
+        correctAns:"Canberra"
+    },
+
+    {
+        question:"What language is spoken in Brazil?",
+        answers:["Portuguese", "Spanish", "English", "Latin"],
+        correctAns:"Portuguese"
+    },
+
+    {
+        question:"What company is also the name of one of the longest rivers in the world?",
+        answers:["Ebay", "Yellow", "Amazon", "Nike"],
+        correctAns:"Amazon"
+    },
+
+    {
+        question:"What language is spoken in Norway?",
+        answers:["English", "Norse", "Norwegian", "Deutsch"],
+        correctAns:"Norwegian"
+    },
+
+    {
+        question:"What number is a baker’s dozen?",
+        answers:["33", "20", "12", "13"],
+        correctAns:"13"
+    },
+
+    {
+        question:"Which planet has the most moons?",
+        answers:["Jupiter", "Saturn", "Neptune", "Mars"],
+        correctAns:"Saturn"
+    },
+
+    {
+        question:"Where is the smallest bone in the human body located?",
+        answers:["nail", "toe", "ear", "nose"],
+        correctAns:"ear"
+    },
+
+    {
+        question:"How many hearts does an octopus have?",
+        answers:["1", "2", "3", "4"],
+        correctAns:"3"
+    },
+
+    {
+        question:"With over 222 million units sold, what is Apple’s highest-selling iPhone model?",
+        answers:["iPhone 10", "iPhone 8", "iPhone 6", "iPhone 4"],
+        correctAns:"iPhone 6"
+    },
+
+    {
+        question:"In which year was the Microsoft XP operating system released?",
+        answers:["2001", "1999", "2002", "1998"],
+        correctAns:"2001"
+    },
+
+    {
+        question:"Natasha Romanova is the real name of which superhero?",
+        answers:["Cat Woman", "Wonder Woman", "Captain Marvel", "Black Widow"],
+        correctAns:"Black Widow"
+    },
+
+    {
+        question:"What is the capital of Canada?",
+        answers:["Calgary", "Montreal", "Toronto", "Ottawa"],
+        correctAns:"Ottawa"
+    },
+
+    {
+        question:"A DNA molecule is described as being what shape?",
+        answers:["Double helix", "square", "circular", "U-shaped"],
+        correctAns:"Double helix"
+    },
+
+    {
+        question:"Which process converts sugar to acids, alcohol or gases?",
+        answers:["alcoholization", "Fermentation", "crystallization", "Pollination"],
+        correctAns:"Fermentation"
+    },
+
+    {
+        question:"Which chamber of the heart pumps deoxygenated blood to the lungs?",
+        answers:["Right ventricle", "Left ventricle", "Pulmonary Artery", "Right Atrium"],
+        correctAns:"Right ventricle"
+    },
+
+    {
+        question:"How many bones are there in the average adult human body?",
+        answers:["523", "102", "1115", "206"],
+        correctAns:"206"
+    },
+
+    {
+        question:"What percentage of the Earth’s surface is made up of the Atlantic Ocean?",
+        answers:["80 per cent", "50 per cent", "20 per cent", "30 per cent"],
+        correctAns:"20 per cent"
+    },
+
+    {
+        question:"What colour is found on 75% of the world’s flags?",
+        answers:["green", "yellow", "blue", "red"],
+        correctAns:"red"
+    },
+
+    {
+        question:"Which film was the first to be recognised as part of the Marvel Cinematic Universe?",
+        answers:["Batman", "Spiderman", "Iron Man", "Black Panther"],
+        correctAns:"Iron Man"
+    },
+
+    {
+        question:"What colour is a giraffe's tongue?",
+        answers:["Green", "Blue", "Red", "Orange"],
+        correctAns:"Blue"
+    },
+
+    {
+        question:"In which century did Leonardo da Vinci paint The Last Supper?",
+        answers:["Fifteenth century", "Seventh century", "Fourteenth century", "Eighteenth century"],
+        correctAns:"Fifteenth century"
+    },
+
+    {
+        question:"Which sign of the Zodiac is represented by the Scales?",
+        answers:["Virgo", "Gemini", "Capricorn", "Libra"],
+        correctAns:"Libra"
+    },
+
+    {
+        question:"In what year was the first Playstation console released?",
+        answers:["2000", "1994", "1996", "1990"],
+        correctAns:"1994"
+    },
+
+    {
+        question:"What is the strongest sense in humans?",
+        answers:["Smell", "Sight", "Hearing", "Taste"],
+        correctAns:"Smell"
+    },
+
+    {
+        question:"What is the highest-grossing Pixar film? ",
+        answers:["Brave", "Cars 3", "Toy Story 4", "The Incredibles 2"],
+        correctAns:"The Incredibles 2"
+    },
+
+    {
+        question:"112.	How old do you have to be to stand for US president?",
+        answers:["35-years-old", "25-years-old", "40-years-old", "15-years-old"],
+        correctAns:"35-years-old"
+    },
+
+    {
+        question:"How much does the average male hippo weigh (in kg)?",
+        answers:["500-900kg", "1,500-1,800kg", "2,500-2800kg", "10,000-11,000kg"],
+        correctAns:"1,500-1,800kg"
+    },
+
+    {
+        question:"What is the chemical symbol for gold?",
+        answers:["Na", "Si", "Au", "Li"],
+        correctAns:"Au"
+    },
+
+    {
+        question:"Which is the only vowel not used as the first letter in a US State?",
+        answers:["A", "E", "I", "O"],
+        correctAns:"E"
+    },
+
+    {
+        question:"To the tip - how tall is the Eiffel Tower?",
+        answers:["170 m", "856 m", "324 m", "502 m"],
+        correctAns:"324 m"
+    },
+
+    {
+        question:"Which bird can fly backwards?",
+        answers:["Hummingbird", "eagle", "owl", "vulture"],
+        correctAns:"Hummingbird"
+    },
+
+    {
+        question:"What name is given to an adult female sheep?",
+        answers:["Ewe", "She-sheep", "Lamb", "Emel"],
+        correctAns:"Ewe"
+    },
+
+    {
+        question:"What year did the Titanic sink in the Atlantic Ocean on 15 April?",
+        answers:["1900", "1945", "1912", "1962"],
+        correctAns:"1912"
+    },
+
+    {
+        question:"How many breaths does the human body take daily?",
+        answers:["500,000 daily", "1,500 daily", "20,000 daily", "9,000 daily"],
+        correctAns:"20,000 daily"
+    },
+
+    {
+        question:"What is the lifespan of a dragonfly? ",
+        answers:["1 week", "1 year", "24 hours", "5 days"],
+        correctAns:"24 hours"
+    },
+
+    {
+        question:"If the Earth were made into a black hole, what would be the diameter of its event horizon?",
+        answers:["20mm", "1km", "20cm", "20km"],
+        correctAns:"20mm"
+    },
+
+    {
+        question:"What is the furthest you can see with the naked eye?",
+        answers:["10km", "50km", "5km", "2.5 million light years"],
+        correctAns:"2.5 million light years"
+    },
+
+    {
+        question:"To the nearest thousand, how many hairs are there on a typical human head?",
+        answers:["1,000,000 hairs", "200 hairs", "10,000 hairs", "45,000 hairs"],
+        correctAns:"10,000 hairs"
     }
 
 ]
